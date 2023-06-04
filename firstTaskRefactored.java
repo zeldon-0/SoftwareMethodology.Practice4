2d23eac42eaf362b0579c6b7c18634876b3af870
Vadym Lavrinenko <vadym.lavrinenko@ihsmarkit.com>
2023-06-04 13:47:11 +0300

    Add project files.

---
+++ firstTaskRefactored.java
@@ -0,0 +1,15 @@
+﻿private static final Set<String> SUSPICIOUS_NAMES = Set.of(
+	"Don", "John"
+);
+
+void checkSecurity(String[] people)
+{
+	for (String person : people)
+	{
+		if (SUSPICIOUS_NAMES.contains(person))
+		{
+			sendAlert(); 
+			return;
+		}
+	}
+}
